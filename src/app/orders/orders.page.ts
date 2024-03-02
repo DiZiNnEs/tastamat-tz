d51c19dd54d3e0eaa32f6500a2ab18d7521687d2
dizinnes <dizinnes228@gmail.com>
2024-03-02 17:15:16 +0500

    refactor: change top bar in orders

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonLabel, IonTabButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { timeOutline, chevronForwardOutline } from 'ionicons/icons';
+
 import { ExploreContainerComponent } from '../components/explore-container/explore-container.component';
 
 @Component({
     selector: 'app-orders-page',
     templateUrl: 'orders.page.html',
+    styleUrls: ['orders.page.scss'],
     standalone: true,
-    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+    imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonIcon, IonLabel, IonTabButton],
 })
 export class OrdersPage {
-    constructor() { }
+    constructor() {
+        addIcons({ timeOutline, chevronForwardOutline })
+    }
 }
