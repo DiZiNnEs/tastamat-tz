61d0928c8bd50479b6ddbce38f5b1c043a2298fd
dizinnes <dizinnes228@gmail.com>
2024-03-03 18:01:54 +0500

    feat: add button to back in get package page

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -12,22 +12,24 @@ import {
   IonItem,
   IonNote,
   IonBadge,
-  IonText
+  IonText, IonNavLink
 } from '@ionic/angular/standalone';
 import {RouterLink} from "@angular/router";
 import { addIcons } from 'ionicons';
 import { timeOutline, chevronForwardOutline, arrowUpCircleOutline } from 'ionicons/icons';
 
 import { ExploreContainerComponent } from '../components/explore-container/explore-container.component';
+import {GetPackagePage} from "../pages/get-package/get.package.page";
 
 @Component({
     selector: 'app-orders-page',
     templateUrl: 'orders.page.html',
     styleUrls: ['orders.page.scss'],
     standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonIcon, IonLabel, IonTabButton, IonList, IonItem, IonNote, IonBadge, IonText, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonIcon, IonLabel, IonTabButton, IonList, IonItem, IonNote, IonBadge, IonText, RouterLink, IonNavLink],
 })
 export class OrdersPage {
+    component = GetPackagePage
     constructor() {
         addIcons({ timeOutline, chevronForwardOutline, arrowUpCircleOutline })
     }
--- src/app/pages/get-package/get.package.page.ts
+++ src/app/pages/get-package/get.package.page.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import {
-  IonApp, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol,
-  IonContent, IonGrid,
+  IonApp,
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
   IonHeader,
-  IonIcon, IonImg, IonInput, IonItem, IonLabel, IonList,
-  IonRouterOutlet, IonRow, IonSelect, IonSelectOption,
+  IonIcon,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonRouterOutlet,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
   IonText,
   IonTitle,
   IonToolbar
@@ -18,7 +36,7 @@ import {addIcons} from "ionicons";
   templateUrl: 'get.package.page.html',
   styleUrls: ['get.package.page.scss'],
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, ExploreContainerComponent, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonText, IonList, IonItem, IonSelect, IonSelectOption, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonImg, IonLabel, IonInput, IonButton],
+  imports: [IonApp, IonRouterOutlet, ExploreContainerComponent, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonText, IonList, IonItem, IonSelect, IonSelectOption, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonImg, IonLabel, IonInput, IonButton, IonBackButton, IonButtons],
 })
 export class GetPackagePage {
   constructor() {
