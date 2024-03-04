abc7028d4dc3742b17ee82118dc49fe2854e868f
dizinnes <dizinnes228@gmail.com>
2024-03-04 19:52:38 +0500

    feat: create order page

---
+++ src/app/pages/order/order.page.ts
@@ -0,0 +1,39 @@
+import {Component, OnDestroy, OnInit,} from "@angular/core";
+import {IonHeader, IonTitle, IonToolbar} from "@ionic/angular/standalone";
+import {HeaderComponent} from "../../components/header/header.component";
+import {IonicModule} from "@ionic/angular";
+import {ActivatedRoute} from "@angular/router";
+@Component({
+  selector: 'app-order-page',
+  templateUrl: 'order.page.html',
+  styleUrls: ['order.page.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    HeaderComponent,
+    IonicModule
+  ],
+  standalone: true
+})
+export class OrderPage implements OnInit, OnDestroy{
+  id = 0;
+  private sub: any;
+
+  title = 'Мои посылки'
+  headerOpt = {
+    isActive: true,
+    href: '/main/orders'
+  }
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => this.id = +params['id'])
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
+  }
+}
